16ba85dc76baafaea97a344d898e68466eecc48c
jjbaker4 <jjbaker4@asu.edu>
2021-06-01 20:56:09 -0400

    Try-catch added to multiply code block to handle invalid requests

--- src/main/java/funHttpServer/WebServer.java
+++ src/main/java/funHttpServer/WebServer.java
@@ -196,29 +196,40 @@ class WebServer {
         } else if (request.contains("multiply?")) {
           // This multiplies two numbers, there is NO error handling, so when
           // wrong data is given this just crashes
-          Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-          // extract path parameters
-          
-          
-          //removes "multiply?" from request, and passes to splitQuery
-          query_pairs = splitQuery(request.replace("multiply?", ""));
-          //query_pairs should now be a map with a pair of strings
-          
-          // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
-          
-          // do math
-          Integer result = num1 * num2;
-
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
-
-          // TODO: Include error handling here with a correct error code and
-          // a response that makes sense
+          try {
+	          Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+	          // extract path parameters
+	          
+	          
+	          //removes "multiply?" from request, and passes to splitQuery
+	          query_pairs = splitQuery(request.replace("multiply?", ""));
+	          //query_pairs should now be a map with a pair of strings
+	          
+	          // extract required fields from parameters
+	          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+	          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+	          
+	          // do math
+	          Integer result = num1 * num2;
+	
+	          // Generate response
+	          builder.append("HTTP/1.1 200 OK\n");
+	          builder.append("Content-Type: text/html; charset=utf-8\n");
+	          builder.append("\n");
+	          builder.append("Result is: " + result);
+	
+	          // TODO: Include error handling here with a correct error code and
+	          // a response that makes sense
+          }
+          catch (Exception e) {
+        	  //build response
+        	  builder.append("HTTP/1.1 400 Bad Syntax\n");
+	          builder.append("Content-Type: text/html; charset=utf-8\n");
+	          builder.append("\n");
+	          builder.append("Please check the input format of the request.\n" + 
+	          "the correct format is:\n/multiply?num1=A&num2=B\nwhere A and B" +
+	        		  "are the numbers you wish to multiply");
+          }
 
         } else if (request.contains("github?")) {
           // pulls the query from the request and runs it with GitHub's REST API
